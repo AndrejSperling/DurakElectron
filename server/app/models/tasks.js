cdd969e700e2f0b8edd9062ca227be8f70e58ddc
Andrej <sperling@protonmail.com>
2016-12-07 12:54:33 +0100

    Ordnerstruktur für unser Projekt

---
+++ server/app/models/tasks.js
@@ -0,0 +1,162 @@
+var TinyTaskDB  = require("tinytaskdb"),
+    mapper      = require('./mapper'),
+    ObjectId    = require('mongodb').ObjectID;
+
+exports.findById = function (id, cb) {
+
+    var isValidObjectId = (id.match(/^[0-9a-fA-F]{24}$/));
+    if(!isValidObjectId) return cb(new Error("Invalid ObjectId", 400), null);
+
+    TinyTaskDB.Task.findOne({'_id': id}, function (err, task) {
+        if (!err && task) {
+            cb(err, mapper.convertTaskToJsonResponse(task))
+        } else {
+            cb(err, null);
+        }
+    });
+};
+
+exports.findAll = function (cb) {
+    TinyTaskDB.Task.find({}, function (err, tasks) {
+        var taskMap = {};
+
+        tasks.forEach(function (task) {
+            taskMap[task._id] = mapper.convertTaskToJsonResponse(task);
+        });
+
+        cb(err, taskMap)
+    });
+};
+
+exports.filterBy = function (filter, cb) {
+
+    // TODO filter
+
+    TinyTaskDB.Task.find({}, function (err, tasks) {
+        var taskMap = {};
+
+        tasks.forEach(function (task) {
+            taskMap[task._id] = mapper.convertTaskToJsonResponse(task);
+        });
+
+        cb(err, taskMap)
+    });
+};
+
+
+exports.saveFromJson = function (userid, json, cb) {
+
+        var newTask = new TinyTaskDB.Task({
+            _id: ObjectId(),
+            createdBy: userid,
+            assignedTo: null,
+            name: json.name,
+            description: json.description,
+            payment: json.payment,
+            position: {
+                latitude: json.position.latitude,
+                longitude: json.position.longitude
+            },
+            starts: json.starts,
+            category: json.category
+        });
+
+        newTask.save(function (err, task) {
+            if (err) return cb(err, null);
+            return cb(err, task._id);
+        });
+
+};
+
+exports.deleteById = function (userid, taskid, cb) {
+    this.findById(taskid, function(err, task){
+
+        if(err)
+            return cb(err, null);
+
+        if(task.createdBy === userid) {
+            TinyTaskDB.Task.findByIdAndRemove(taskid, cb);
+        }else{
+            return cb(new Error("You are not the owner!", 400), null)
+        }
+
+    });
+};
+
+exports.updateFromJson = function (taskid, json, cb) {
+
+    TinyTaskDB.Task.findOneAndUpdate(
+        {'_id': taskid},
+        {$set: json},
+        function (err, task) {
+            if (err) return cb(err, null);
+            return cb(err, task);
+        });
+};
+
+
+exports.findPosition = function (id, cb) {
+
+    TinyTaskDB.Task.findOne({'_id': id}, function (err, task) {
+        if (err) {
+            cb(err, null);
+        } else {
+            cb(err, {
+                'lat': task.position.latitude,
+                'lng': task.position.longitude
+            });
+        }
+    });
+
+};
+
+exports.findApplications = function (id, cb) {
+
+    TinyTaskDB.Application.find({'task':id}, function (err, application) {
+
+        if (err) {
+            cb(err, null);
+        } else {
+
+            var applicationMap = {};
+
+            applicationMap["results"] = application.length;
+            applicationMap["applications"] = [];
+            application.forEach(function (application) {
+                applicationMap["applications"].push(mapper.convertApplicationToJsonResponse(application))
+            });
+
+            cb(err, applicationMap);
+        }
+    });
+
+};
+
+exports.saveRatingFromJson = function(json, cb){
+
+    TinyTaskDB.Task.findById(json.task, function (err, task) {
+
+        if(err) return cb(err, null);
+
+        TinyTaskDB.User.findById(json.assignedTo, function(err, user){
+
+            if(err) return cb(err, null);
+
+            var rating = new TinyTaskDB.Rating({
+                assignedTo:     json.assignedTo,
+                task:           json.taskid,
+                isExecutor:     (json.assignedTo === user),
+                value:          json.value,
+                comment:        json.comment
+            });
+
+            rating.save(function (err, rating) {
+                if(err) return cb(err, null);
+                return cb(err, rating)
+            });
+
+        });
+
+    });
+
+};
---
+++ server/server.js
@@ -0,0 +1,44 @@
+var express     = require('express'),
+    app         = express(),
+    config      = require('./app/config.js');
+
+var port        = process.env.PORT || 8080,
+    mongodb     = process.env.MONGO_DB || config.mongodb,
+    mongoose    = require('mongoose'),
+    logger      = require('morgan'),
+    bodyParser  = require('body-parser'),
+    jwt         = require('express-jwt'),
+    routes      = require('./app/routes');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// set json pretty format: 2 in development, 0 in production
+app.set('json spaces', 2);
+
+// authentication
+app.use(jwt({
+    audience: config.auth.clientID,
+    issuer: 'https://' + config.auth.domain + '/',
+    secret: new Buffer(config.auth.clientSecret, 'base64')
+}));
+app.use(require("./app/middlewares/authusercheck"));
+
+// routes
+app.use('/', routes);
+
+// error handling
+app.use(require("./app/middlewares/errors"));
+
+// db connect and create server
+mongoose.Promise = global.Promise;
+mongoose.connect(mongodb, function (err) {
+    if (err) {
+        console.log('Unable to connect to Mongo.')
+        process.exit(1)
+    } else {
+        app.listen(port);
+        console.log('listen to port ' + port);
+    }
+});
