a19955f5b7fd653df4962988d00d8c718a8e28ff
Andrej <sperling@protonmail.com>
2016-12-21 14:01:43 +0100

    View Controller verbessert

--- electron/app/durak/controller.js
+++ electron/app/durak/controller.js
@@ -5,25 +5,71 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 var durak_1 = require("./durak");
-var Controller = (function () {
-    function Controller(document) {
+var $ = require("jquery");
+var ViewController = (function () {
+    function ViewController() {
         this.DATA_SYMBOL = "data-card-symbol";
         this.DATA_VALUE = "data-card-value";
         this.PLAYABLE_CARDS = "playable";
-        this.doc = document;
+        this.PLAYER_VIEW_CONTAINER = "";
+        this.PLAYER_VIEW_AVATAR = "";
+        this.PLAYER_VIEW_CARDS = "";
+        this.DECK_OF_CARDS = "deckOfCards";
+        this.BUTTON_END_MOVE = "endMove";
+        this.BUTTON_TAKE_CARD = "takeCard";
+        this.SHOW_MAX_CARDS = 5;
+        this._cardsOnDeck = 0;
     }
-    return Controller;
+    ViewController.prototype.onLoadView = function (doc) {
+        doc.getElementById(this.BUTTON_END_MOVE).onclick = this.onClickEndMove;
+        doc.getElementById(this.BUTTON_TAKE_CARD).onclick = this.onClickTakeCard;
+        this.deckOfCardsView = doc.getElementById(this.DECK_OF_CARDS);
+    };
+    Object.defineProperty(ViewController.prototype, "cardsOnDeck", {
+        get: function () {
+            return this._cardsOnDeck;
+        },
+        set: function (cards) {
+            console.log("CardsOnDeck");
+            this._cardsOnDeck = cards;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    ViewController.prototype.generateCardsView = function (parent, numberOfCards) {
+        var positionStep = 2;
+        var showCards = (numberOfCards > this.SHOW_MAX_CARDS) ? this.SHOW_MAX_CARDS : numberOfCards;
+        parent.innerHTML = "";
+        for (var i = 0; i < showCards; i++) {
+            var left = positionStep * i;
+            var div = $('<div></div>')
+                .addClass('card')
+                .attr('style', 'left:' + left + '%');
+            div.appendTo(parent);
+        }
+        if (numberOfCards > showCards) {
+            var div = $('<div></div>')
+                .addClass('more')
+                .text(numberOfCards - showCards);
+            div.appendTo(parent);
+        }
+    };
+    ViewController.prototype.renderViews = function () {
+        this.generateCardsView(this.deckOfCardsView, this._cardsOnDeck);
+    };
+    return ViewController;
 }());
-exports.Controller = Controller;
-var AIController = (function (_super) {
-    __extends(AIController, _super);
-    function AIController(document) {
-        _super.call(this, document);
+exports.ViewController = ViewController;
+var AIViewController = (function (_super) {
+    __extends(AIViewController, _super);
+    function AIViewController() {
+        _super.call(this);
         this.game = new durak_1.Durak("WasApBi");
     }
-    AIController.prototype.init = function (doc) {
+    AIViewController.prototype.onLoadView = function (doc) {
+        _super.prototype.onLoadView.call(this, doc);
         var self = this;
-        // init clicklistener for cards
+        // onLoadView clicklistener for cards
         var playableCards = doc.getElementsByClassName(this.PLAYABLE_CARDS);
         for (var i = 0; i < playableCards.length; i++) {
             playableCards[i].addEventListener("click", function () {
@@ -31,13 +77,23 @@ var AIController = (function (_super) {
             });
         }
     };
-    AIController.prototype.playCard = function (clickedElement) {
+    AIViewController.prototype.playCard = function (clickedElement) {
         console.log(clickedElement);
         var symbole = clickedElement.getAttribute(this.DATA_SYMBOL);
         var value = clickedElement.getAttribute(this.DATA_VALUE);
         alert("Clicked: symbole-> " + symbole + " value->" + value);
         clickedElement.remove();
     };
-    return AIController;
-}(Controller));
-exports.AIController = AIController;
+    AIViewController.prototype.onClickTakeCard = function () {
+        this.cardsOnDeck = 5;
+        _super.prototype.renderViews.call(this);
+        _super.prototype.generateCardsView.call(this, this.deckOfCardsView, 5);
+    };
+    AIViewController.prototype.onClickEndMove = function () {
+        this.cardsOnDeck = 5;
+        this.renderViews();
+        _super.prototype.generateCardsView.call(this, this.deckOfCardsView, 5);
+    };
+    return AIViewController;
+}(ViewController));
+exports.AIViewController = AIViewController;
--- electron/app/durak/controller.ts
+++ electron/app/durak/controller.ts
@@ -1,30 +1,92 @@
 import {Durak} from "./durak";
+import * as $ from "jquery";
 
-export class Controller {
+export abstract class ViewController {
 
     protected readonly DATA_SYMBOL = "data-card-symbol";
     protected readonly DATA_VALUE = "data-card-value";
     protected readonly PLAYABLE_CARDS = "playable";
-    protected doc;
 
-    constructor(document: HTMLDocument) {
-        this.doc = document;
+    protected readonly PLAYER_VIEW_CONTAINER = "";
+    protected readonly PLAYER_VIEW_AVATAR = "";
+    protected readonly PLAYER_VIEW_CARDS = "";
+    protected readonly DECK_OF_CARDS = "deckOfCards";
+
+    protected readonly BUTTON_END_MOVE = "endMove";
+    protected readonly BUTTON_TAKE_CARD = "takeCard";
+
+
+    protected playerViews: Array<HTMLDivElement>;
+
+    private readonly SHOW_MAX_CARDS = 5;
+    protected deckOfCardsView: HTMLElement;
+    private _cardsOnDeck: number = 0;
+
+    constructor() {
+    }
+
+    onLoadView(doc: HTMLDocument) {
+
+        doc.getElementById(this.BUTTON_END_MOVE).onclick = this.onClickEndMove;
+        doc.getElementById(this.BUTTON_TAKE_CARD).onclick = this.onClickTakeCard;
+
+        this.deckOfCardsView = doc.getElementById(this.DECK_OF_CARDS);
+    }
+
+    abstract onClickEndMove()
+
+    abstract onClickTakeCard()
+
+    get cardsOnDeck(): number {
+        return this._cardsOnDeck;
+    }
+
+    set cardsOnDeck(cards: number) {
+        console.log("CardsOnDeck")
+        this._cardsOnDeck = cards;
+    }
+
+    generateCardsView(parent: HTMLElement, numberOfCards: number) {
+        let positionStep = 2;
+        let showCards = (numberOfCards > this.SHOW_MAX_CARDS) ? this.SHOW_MAX_CARDS : numberOfCards;
+        parent.innerHTML = "";
+
+        for (let i = 0; i < showCards; i++) {
+            let left = positionStep * i;
+            let div = $('<div></div>')
+                .addClass('card')
+                .attr('style', 'left:' + left + '%');
+            div.appendTo(parent);
+        }
+
+        if (numberOfCards > showCards) {
+            let div = $('<div></div>')
+                .addClass('more')
+                .text(numberOfCards - showCards);
+            div.appendTo(parent);
+        }
+    }
+
+    protected renderViews(){
+        this.generateCardsView(this.deckOfCardsView, this._cardsOnDeck);
     }
 }
 
-export class AIController extends Controller {
+export class AIViewController extends ViewController {
 
     private game;
 
-    constructor(document: HTMLDocument) {
-        super(document);
+    constructor() {
+        super();
         this.game = new Durak("WasApBi");
     }
 
-    init(doc: HTMLDocument) {
+    onLoadView(doc: HTMLDocument) {
+        super.onLoadView(doc);
+
         let self = this;
 
-        // init clicklistener for cards
+        // onLoadView clicklistener for cards
         let playableCards = doc.getElementsByClassName(this.PLAYABLE_CARDS);
         for (let i = 0; i < playableCards.length; i++) {
             playableCards[i].addEventListener("click", function () {
@@ -44,6 +106,19 @@ export class AIController extends Controller {
         clickedElement.remove()
     }
 
+
+    onClickTakeCard() {
+        this.cardsOnDeck = 5;
+        super.renderViews();
+        super.generateCardsView(this.deckOfCardsView, 5);
+    }
+
+    onClickEndMove() {
+        this.cardsOnDeck = 5;
+        this.renderViews();
+
+        super.generateCardsView(this.deckOfCardsView, 5);
+    }
 }
 
 
